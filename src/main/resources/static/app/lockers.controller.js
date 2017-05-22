d7d35381607ec8b469921128edfa0685bcaf98fa
Eddy <edcapitano14@gmail.com>
2017-05-21 21:54:42 -0400

    librerias pickadate

---
+++ src/main/resources/static/app/lockers.controller.js
@@ -0,0 +1,16 @@
+(function(){
+	'use strict';
+	
+	angular.module('app')
+	.controller('LockersController',LockersController);
+	
+	LockersController.$inject=['$timeout','$compile','$http','$scope'];
+	
+	function LockersController($timeout,$compile,$http,$scope){
+		var vm = this;
+		
+	}
+	
+	
+})();
+		
