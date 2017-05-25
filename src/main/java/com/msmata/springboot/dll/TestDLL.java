9bb9b02be29bb6d7916123d72436d4687a962b87
Pc2015 <Pc2015@DESKTOP-R7B2IOO>
2017-05-24 23:02:44 -0400

    .

--- src/main/java/com/msmata/springboot/dll/TestDLL.java
+++ src/main/java/com/msmata/springboot/dll/TestDLL.java
@@ -2,11 +2,11 @@ package com.msmata.springboot.dll;
 
 public class TestDLL {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		
 		int resultado = WS.INSTANCE.WS_Init();
 		System.out.println("resultado = " + resultado);
 		
 		System.exit(0);
-	}
+	}*/
 }
